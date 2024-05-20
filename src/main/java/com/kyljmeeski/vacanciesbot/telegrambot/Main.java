49110204b28a96443a6631e7a2beadea70b63acb
kyljmeeski <kyljmeeski@gmail.com>
2024-05-20 21:56:59 +0600

    feat: establish connection to postgres

--- src/main/java/com/kyljmeeski/vacanciesbot/telegrambot/Main.java
+++ src/main/java/com/kyljmeeski/vacanciesbot/telegrambot/Main.java
@@ -4,13 +4,21 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
 
     public static void main(String[] args) {
+        String url = "jdbc:postgresql://localhost:5432/vacancies-bot";
+        String user = "postgres";
+        String password = "postgres";
         try {
+            Connection connection = DriverManager.getConnection(url, user, password);
             TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
-            botsApi.registerBot(new VacanciesBot());
-        } catch (TelegramApiException e) {
+            botsApi.registerBot(new VacanciesBot(connection));
+        } catch (TelegramApiException | SQLException e) {
             e.printStackTrace();
         }
     }
