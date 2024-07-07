01f6ffc1303caba0ce7326c07b60276d2de08f00
kyljmeeski <kyljmeeski@gmail.com>
2024-07-07 16:32:18 +0600

    feat: establish rabbitmq connection and start consuming queue telegram-messages to send them to users

--- src/main/java/com/kyljmeeski/vacanciesbot/telegrambot/Main.java
+++ src/main/java/com/kyljmeeski/vacanciesbot/telegrambot/Main.java
@@ -23,26 +23,53 @@
  */
 package com.kyljmeeski.vacanciesbot.telegrambot;
 
+import com.kyljmeeski.rabbitmqwrapper.Consumer;
+import com.kyljmeeski.rabbitmqwrapper.PlainConsumer;
+import com.kyljmeeski.rabbitmqwrapper.Queues;
+import com.kyljmeeski.rabbitmqwrapper.RabbitQueue;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
-import java.sql.Connection;
+import java.io.IOException;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.concurrent.TimeoutException;
 
 public class Main {
 
     public static void main(String[] args) {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        factory.setPort(5672);
+
         String url = "jdbc:postgresql://localhost:5432/vacancies-bot";
         String user = "postgres";
         String password = "postgres";
+
+        VacanciesBot bot = null;
+
         try {
-            Connection connection = DriverManager.getConnection(url, user, password);
+            java.sql.Connection connection = DriverManager.getConnection(url, user, password);
             TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
-            botsApi.registerBot(new VacanciesBot(connection));
+            bot = new VacanciesBot(connection);
+            botsApi.registerBot(bot);
         } catch (TelegramApiException | SQLException e) {
             e.printStackTrace();
+        }
+
+        try {
+            Connection rabbitMQConnection = factory.newConnection();
+            Queues queues = new Queues(rabbitMQConnection);
+            RabbitQueue queue = queues.declare(
+                    "telegram-messages", false, false, false, null
+            );
+            Consumer consumer = new PlainConsumer(rabbitMQConnection, queue, new SendTelegramMessageJob(bot));
+            consumer.startConsuming();
+        } catch (IOException | TimeoutException e) {
+            throw new RuntimeException(e);
         }
     }
 
